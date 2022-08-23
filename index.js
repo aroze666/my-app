cbef2c0520402c0ad1e18f8ef74852eb2d63e542
aroze666 <aroze666@gmail.com>
2022-08-23 15:31:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { response } = require("express");
+const express = require("express");
+const { readFile } = require("fs");
+const app = express();
+app.get("/", (req, res) => {
+  readFile("./index.html", "utf8", (err, html) => {
+    if (err) {
+      response.status(500).send("Error 500");
+    }
+    res.send(html);
+  });
+});
+app.get("/primality.html", (req, res) => {
+  readFile("./primality.html", "utf8", (err, html) => {
+    if (err) {
+      response.status(500).send("Error 500");
+    }
+    res.send(html);
+  });
+});
+app.get("/collatz.html", (req, res) => {
+  readFile("./collatz.html", "utf8", (err, html) => {
+    if (err) {
+      response.status(500).send("Error 500");
+    }
+    res.send(html);
+  });
+});
+app.get("/music.html", (req, res) => {
+  readFile("./music.html", "utf8", (err, html) => {
+    if (err) {
+      response.status(500).send("Error 500");
+    }
+    res.send(html);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(port);
+});
