b6eac0daf0ce297ef2310d3cf403865d3bf9a2b0
aroze666 <aroze666@gmail.com>
2022-08-24 22:41:27 -0700

    added harmony section

--- src/music.js
+++ src/music.js
@@ -1,6 +1,8 @@
 const play = document.getElementById("play");
 const pitch = document.getElementById("pitch");
-
+const frequency = document.getElementById("display_pitch");
+const notes = document.getElementsByClassName("note");
+const harmony = document.getElementById("harmony");
 const audioContext = new AudioContext();
 const buffer = audioContext.createBuffer(
   1,
@@ -26,3 +28,46 @@ play.addEventListener("click", () => {
   oscillator.start();
   oscillator.stop(audioContext.currentTime + 1);
 });
+pitch.addEventListener("change", () => {
+  frequency.innerHTML = "Frequency: " + pitch.value;
+});
+
+let playNote = [false, false, false, false];
+
+for (let i = 0; i < 4; i++) {
+  notes[i].addEventListener("change", (e) => {
+    if (e.target.checked) {
+      playNote[i] = true;
+    } else {
+      playNote[i] = false;
+    }
+  });
+}
+
+harmony.addEventListener("click", () => {
+  for (let i = 0; i < 4; i++) {
+    if (playNote[i]) {
+      const oscillator = audioContext.createOscillator();
+      oscillator.frequency.setValueAtTime(
+        playPitch(i),
+        audioContext.currentTime
+      );
+      oscillator.connect(primaryGainControl);
+      oscillator.start();
+      oscillator.stop(audioContext.currentTime + 1);
+    }
+  }
+});
+
+function playPitch(num) {
+  switch (num) {
+    case 0:
+      return 440;
+    case 1:
+      return 554.37;
+    case 2:
+      return 659.25;
+    case 3:
+      return 880;
+  }
+}
